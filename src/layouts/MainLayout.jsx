d45cd5640f56ec2a66845b51306691527931983a
Vahe <ohanyanv703@gmail.com>
2025-08-14 00:40:47 +0400

    Main Layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,15 @@
-import {Outlet }from "react-router-dom"
-import Navbar from "../components/Navbar"; 
-import  Footer  from "../components/Footer";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 const MainLayout = () => {
-    return (
-        <div className="bg-zinc-100">
-            <Navbar />
-            <div>
-            <Outlet />  
-
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="bg-zinc-100">
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default MainLayout;
