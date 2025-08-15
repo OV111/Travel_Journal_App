c0764e6483706717b47cbef9cec04bf37f93d7a7
Vahe <ohanyanv703@gmail.com>
2025-08-15 20:17:47 +0400

    AddTrip and MyJournal

---
+++ src/pages/AddTrip.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AddTrip = () => {
+    return (
+        <p>Add Trip</p>
+    )
+}
+export default AddTrip
---
+++ src/pages/MyJournal.jsx
@@ -0,0 +1,6 @@
+const MyJournal = () => {
+    return (
+        <p>My Journal</p>
+    )
+}
+export default MyJournal;
