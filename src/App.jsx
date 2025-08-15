b2f623dad7c48004ba49d33ae54ca85f9c55c768
Vahe <ohanyanv703@gmail.com>
2025-08-15 14:50:59 +0400

    App main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
-import AuthProvider from "./Context/AuthContext";
+import AuthProvider from "./Context/AuthProvider";
 import Login from "./pages/Login";
 import MyJournal from "./pages/MyJournal";
 import AddTrip from "./pages/AddTrip";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 const App = () => {
-  // const [login,setLogin] = useState(true)
-
-  // if(login) {
-
-  // } else {
-
-  // }
   return (
     <AuthProvider>
       <BrowserRouter>
@@ -27,10 +19,24 @@ const App = () => {
             <Route path="/explore" element={<Explore />} />
             <Route path="/login" element={<Login />} />
           </Route>
+          
           <Route element={<MainLayout />}>
-
-            <Route path="/my-journal" element={<ProtectedRoutes><MyJournal /></ProtectedRoutes>} />
-            <Route path="/add-trip" element={<AddTrip />} />
+            <Route
+              path="/my-journal"
+              element={
+                <ProtectedRoutes>
+                  <MyJournal />
+                </ProtectedRoutes>
+              }
+            />
+            <Route
+              path="/add-trip"
+              element={
+                <ProtectedRoutes>
+                  <AddTrip />
+                </ProtectedRoutes>
+              }
+            />
           </Route>
 
           <Route path="*" element="Not Found!"></Route>
