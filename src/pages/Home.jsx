922b5eb48492860c8299e9dac35acef554e3eb86
Vahe <ohanyanv703@gmail.com>
2025-08-17 20:19:45 +0400

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,57 @@
-import React from "react";
-// import { useState } from "react";
-import Navbar from "../components/Navbar";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
+import { TripsContext } from "../Context/TripsContext";
 import SplitText from "../components/SplitText";
+
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
 const handleAnimationComplete = () => {
-  console.log('all leters have been animated!')
-}
+  console.log("All letters have been animated!");
+};
+
 const Home = () => {
-  // const { isAuthenticated, username, password, login } = useContext(AuthContext);
+  const { isAuthenticated, addTrip } = useContext(AuthContext);
+  const { posts } = useContext(TripsContext);
+
+  // const nextSlide = () => {};
+
+  // const prevSlide = () => {};
+
+  useEffect(() => {}, [posts.length]);
 
   return (
     <React.Fragment>
-      <div>
-        <main className="container mx-auto px-4 py-8">
-          <div className="text-center mb-12">
-            <SplitText
-              text="Welcome to Travel Journal!"
-              className="text-5xl font-semibold text-center mb-4"
-              delay={100}
-              duration={0.5}
-              ease="power3.out"
-              splitType="chars"
-              from={{ opacity: 0, y: 40 }}
-              to={{ opacity: 1, y: 0 }}
-              threshold={0.1}
-              rootMargin="-100px"
-              textAlign="center"
-              onLetterAnimationComplete={handleAnimationComplete}
-            />
-            <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
-              Discover amazing travel experiences from fellow adventurers and
-              share your own journey with the world!
-            </p>
-          </div>
-        </main>
-      </div>
+      <main className="container mx-full px-4 py-8">
+        <div className="text-center mb-12">
+          <SplitText
+            text="Welcome to Travel Journal!"
+            className="text-5xl font-semibold mb-4"
+            delay={100}
+            duration={0.5}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
+          <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
+            Discover amazing travel experiences from fellow adventurers and
+            share your own journey with the world!
+          </p>
+        </div>
+      </main>
     </React.Fragment>
   );
 };
+
 export default Home;
