fd9fd318d149f9b85626fce8dc7099194a2b2efe
Vahe <ohanyanv703@gmail.com>
2025-08-17 20:20:59 +0400

    Trips provider

--- src/Context/TripsProvider.jsx
+++ src/Context/TripsProvider.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TripsContext } from "./TripsContext";
-
+import { postsObj as initialPosts } from "../data/posts";
 const TripsProvider = ({ children }) => {
-  const [trips, setTrips] = useState([]);
+  const [posts,setPosts] = useState(initialPosts)
+  const [trips, setTrips] = useState(() => {
+    const savedTrips = localStorage.getItem("trips");
+    return savedTrips ? JSON.parse(savedTrips) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("trips", JSON.stringify(trips));
+  }, [trips]);
+
   const addTrip = (trip) => {
-    setTrips((prev) => [...prev, trip]);
+    setTrips((prev) => {
+      if (!prev.find((t) => t.id === trip.id)) return [...prev, trip];
+      return prev;
+    });
+
+    setPosts((prev) => {
+      if (!prev.find((p) => p.id === trip.id)) return [...prev, trip];
+      return prev;
+    });
   };
+
   const deleteTrip = () => {};
 
   return (
-    <TripsContext.Provider value={{ trips, addTrip, deleteTrip }}>
+    <TripsContext.Provider value={{posts, trips, addTrip, deleteTrip }}>
       {children}
     </TripsContext.Provider>
   );
