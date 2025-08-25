7dab1a5eb4621e07b87789d5ad74f4bc43082faa
Vahe <ohanyanv703@gmail.com>
2025-08-25 23:38:14 +0400

    added RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,65 @@
-// React.DataBase Firebase
-// nayel Travel Journal i hamar
 // user Authenticationy
-// Ui y Perfect sarqel
 // Logout y Ui i tarbervox sarqel
-import React, { lazy, Suspense } from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import MainLayout from "./layouts/MainLayout";
-import ProtectedRoutes from "./components/ProtectedRoutes";
+import React, { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./Context/AuthProvider";
 import TripsProvider from "./Context/TripsProvider";
-import Login from "./pages/Login";
-import NotFound from "./components/NotFound";
-import LoaderSpinner from "./components/LoaderSpinner";
 
-const Explore = lazy(() => import("./pages/Explore"));
-const MyJournal = lazy(() => import("./pages/MyJournal"));
-const AddTrip = lazy(() => import("./pages/AddTrip"));
-const ReadMore = lazy(() => import("./pages/ReadMore"));
+import LoaderSpinner from "./components/LoaderSpinner";
+import router from "./router";
 
 const App = () => {
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <AuthProvider>
-          <TripsProvider>
-            <Suspense fallback={<LoaderSpinner />}>
-              <Routes>
-                <Route element={<MainLayout />}>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/explore" element={<Explore />} />
-                  <Route path="/explore/:id" element={<ReadMore />} />
-                  <Route path="/login" element={<Login />} />
-
-                  <Route
-                    path="/my-journal"
-                    element={
-                      <ProtectedRoutes>
-                        <MyJournal />
-                      </ProtectedRoutes>
-                    }
-                  />
-                  <Route
-                    path="/add-trip"
-                    element={
-                      <ProtectedRoutes>
-                        <AddTrip />
-                      </ProtectedRoutes>
-                    }
-                  />
-                </Route>
-
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </TripsProvider>
-        </AuthProvider>
-      </BrowserRouter>
+      <AuthProvider>
+        <TripsProvider>
+          <Suspense fallback={<LoaderSpinner />}>
+            <RouterProvider router={router} />
+          </Suspense>
+        </TripsProvider>
+      </AuthProvider>
     </React.Fragment>
   );
+
+  // return (
+  //   <React.Fragment>
+  //     <BrowserRouter>
+  //       <AuthProvider>
+  //         <TripsProvider>
+  //           <Suspense fallback={<LoaderSpinner />}>
+  //             <Routes>
+  //               <Route element={<MainLayout />}>
+  //                 <Route path="/" element={<Home />} />
+  //                 <Route path="/explore" element={<Explore />} />
+  //                 <Route path="/explore/:id" element={<ReadMore />} />
+  //                 <Route path="/login" element={<Login />} />
+
+  //                 <Route
+  //                   path="/my-journal"
+  //                   element={
+  //                     <ProtectedRoutes>
+  //                       <MyJournal />
+  //                     </ProtectedRoutes>
+  //                   }
+  //                 />
+  //                 <Route
+  //                   path="/add-trip"
+  //                   element={
+  //                     <ProtectedRoutes>
+  //                       <AddTrip />
+  //                     </ProtectedRoutes>
+  //                   }
+  //                 />
+  //               </Route>
+
+  //               <Route path="*" element={<NotFound />} />
+  //             </Routes>
+  //           </Suspense>
+  //         </TripsProvider>
+  //       </AuthProvider>
+  //     </BrowserRouter>
+  //   </React.Fragment>
+  // );
 };
 
 export default App;
