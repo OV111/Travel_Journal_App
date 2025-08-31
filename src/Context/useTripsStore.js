e3a5d9627d23d7c16f992af039901b31a4b141c1
Vahe <ohanyanv703@gmail.com>
2025-08-31 15:47:49 +0400

    Trips Store with zustand(changable

---
+++ src/Context/useTripsStore.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { create } from "zustand";
+// import apiService from "../service/api";
+import tripService from "../service/tripsService";
+
+const useTripsStore = create((set,get) => ({
+  posts: [],
+  journal: JSON.parse(localStorage.getItem("journalTrips")) || [],
+  fetchPosts: async () => {
+    try {
+      let data = await tripService.getTrips()
+      set({ posts: data });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+
+  addTrip: (trip) => {
+    const { journal, posts } = get();
+
+    if (!journal.find((t) => t.id === trip.id)) {
+      const updatedTrips = [...journal, trip];
+      localStorage.setItem("journalTrips", JSON.stringify(updatedTrips));
+      set({ journal: updatedTrips });
+    }
+    if (!posts.find((p) => p.id === trip.id)) {
+      set({ posts: [...posts, trip] });
+    }
+  },
+  deleteTrip: (trip) => {
+
+},
+}));
+export default useTripsStore;
