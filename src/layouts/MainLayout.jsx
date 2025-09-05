b11ed245e0c57275d6830498ff35ef4dc7fe9461
Vahe <ohanyanv703@gmail.com>
2025-09-05 15:36:32 +0400

    Main Layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,26 +1,25 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { ToastContainer } from "react-toastify";
-import  apiService from "../service/api.js"
-import { useEffect } from "react";
+import apiService from "../service/api.js";
+
 const MainLayout = () => {
-    useEffect(() => {
-      async function fetchData() {
-          await apiService.request(
-            "/journals",
-            {},
-            "Fail to load trips!",
-            "Trips successfully loaded!"
-          )
-      }
-      fetchData()
-    },[])
-    // ! only need to show up for Error in case of api 
+  useEffect(() => {
+    async function fetchData() {
+      await apiService.request(
+        "/journals",
+        {},
+        "Fail to load trips!",
+        "Trips successfully loaded!"
+      );
+    }
+    fetchData();
+  }, []);
   return (
     <div className="bg-zinc-100 py-0">
-      
-      <ToastContainer 
+      <ToastContainer
         position="top-center"
         autoClose={2000}
         hideProgressBar={false}
@@ -38,4 +37,5 @@ const MainLayout = () => {
     </div>
   );
 };
+
 export default MainLayout;
