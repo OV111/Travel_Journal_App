f496ccad11ca4246025995da0268c421e283c25b
Vahe <ohanyanv703@gmail.com>
2025-09-06 14:48:10 +0400

    read more page added selectedPost

--- src/pages/ReadMore.jsx
+++ src/pages/ReadMore.jsx
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { initialPosts } from "../data/posts";
+// import { initialPosts } from "../data/posts";
 import { Fullscreen } from "lucide-react";
 import { Link } from "react-router-dom";
+import useTripsStore from "../Context/useTripsStore";
+import NotFound from "../components/NotFound";
 const ReadMore = () => {
   const { id } = useParams();
-  console.log(parseInt(id));
-  const post = initialPosts.find((i) => i.id === parseInt(id));
+  const fetchPostsWithID = useTripsStore().fetchPostsWithID;
+  const selectedPost = useTripsStore().selectedPost;
 
-  if (!post) {
-    return <p>Post Not Founded!</p>;
+  useEffect(() => {
+    fetchPostsWithID(id);
+  }, [id, fetchPostsWithID]);
+
+  if (!selectedPost) {
+    return (
+      <React.Fragment>
+        <NotFound >404 Not Found</NotFound>
+      </React.Fragment>
+    );
   }
+
   return (
     <React.Fragment>
       <div className="w-full min-h-screen bg-background bg-zinc-100 ">
         <div className="relative overflow-hidden">
           <img
-            src={post.image}
-            alt={post.title}
+            src={selectedPost.image}
+            alt={selectedPost.title}
             className="w-full h-[500px] object-cover"
           />
           <div className="absolute inset-0  bg-gradient-to-t from-black/60 via-black/20 to-transparent">
             <h1 className="text-gray-100 text-6xl font-bold pl-10 mt-80  ">
-              {post.title}
+              {selectedPost.title}
             </h1>
             <p className="text-gray-100 text-3xl font-bold pl-11 mt-4">
-              {post.location} • {new Date(post.date).toLocaleDateString()}
+              {selectedPost.location} • {new Date(selectedPost.date).toLocaleDateString()}
             </p>
           </div>
         </div>
         <div>
-          <p className="text-slate-500 text-2xl text-start ml-10 mt-6">{post.shortDescription}</p>
+          <p className="text-slate-500 text-2xl text-start ml-10 mt-6">
+            {selectedPost.shortDescription}
+          </p>
           <h3 className="text-3xl font-medium text-[#003580] ml-10 mt-6">
             A Journey Worth Taking
           </h3>
@@ -45,11 +58,17 @@ const ReadMore = () => {
           </p>
         </div>
 
-         <div className="mt-16 mb-16 ml-[25rem] p-8 bg-card rounded-lg bg-zinc-200 w-[40rem]">
+        <div className="mt-16 mb-16 ml-[25rem] p-8 bg-card rounded-lg bg-zinc-200 w-[40rem]">
           <div className="text-center space-y-4">
-            <h3 className="text-3xl font-serif font-bold text-card-foreground">Ready for Your Next Adventure?</h3>
-            <p className="text-2xl text-muted-foreground">Discover more amazing destinations and travel stories.</p>
-            <button className="font-medium text-xl mt-3  px-6 py-3 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-[#003580] transition-colors duration-300 ease-in-out"><Link to="/explore">Explore More Articles</Link></button>
+            <h3 className="text-3xl font-serif font-bold text-card-foreground">
+              Ready for Your Next Adventure?
+            </h3>
+            <p className="text-2xl text-muted-foreground">
+              Discover more amazing destinations and travel stories.
+            </p>
+            <button className="font-medium text-xl mt-3  px-6 py-3 bg-blue-600 text-white rounded-lg cursor-pointer hover:bg-[#003580] transition-colors duration-300 ease-in-out">
+              <Link to="/explore">Explore More Articles</Link>
+            </button>
           </div>
         </div>
       </div>
